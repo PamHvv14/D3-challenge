a99062ab5e6765c81ff719fd6d446c9af168ebe1
Pamela Hernandez <pamelahernandez@Pamelas-MacBook-Pro.local>
2020-11-16 14:35:17 -0600

    update

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,60 @@
-// @TODO: YOUR CODE HERE!
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function(data) {
+    // Step 4: Parse the data
+    console.log(data);
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([d3.min(data, d => d["age"]) * 0.8,
+        d3.max(data, d => d["age"]) * 1.2
+    ])
+    .range([0, width]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain(0,d3.max(hairData, d => d["smokes"]))
+    .range([height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.age); } )
+      .attr("cy", function (d) { return y(d.smokes); } )
+      .attr("r", 3)
+      .style("fill", "#69b3a2")
+
+})
